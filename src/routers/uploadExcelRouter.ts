d5fff86d341d595f9d060cec258833d1868e2526
Saharat (Aon) Wanthong <noxhsxrk@icloud.com>
2023-07-24 19:48:11 +0700

    feature: add auto create uploads folder when there has had one yet

--- src/routers/uploadExcelRouter.ts
+++ src/routers/uploadExcelRouter.ts
@@ -1,21 +1,26 @@
 import express from "express";
+import fs from "fs";
 import multer from "multer";
 import path from "path";
 import uploadExcel from "../controllers/uploadExcel";
 
 const router = express.Router();
+const uploadsDir = path.join(__dirname, "../../uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-
     const ext = path.extname(file.originalname);
     cb(null, Date.now() + ext);
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage, dest: "uploads/" });
 
 router.post("/upload-excel", upload.single("excelFile"), uploadExcel);
 
